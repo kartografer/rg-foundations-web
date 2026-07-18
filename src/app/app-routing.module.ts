6529b9c14150e43aa97ec9deadec9e877ad72cd6
Joel Guilarte <jguilarte01@gmail.com>
2026-07-18 12:08:47 -0400

    feat: add Friends of RG donation page with routing and styling

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router'
+import { FriendsOfRgComponent } from './features/friends-of-rg/friends-of-rg.component'
 
-export const routes: Routes = []
+export const routes: Routes = [
+  {
+    path: 'donate',
+    component: FriendsOfRgComponent,
+  },
+]
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,24 @@
-import { Component } from '@angular/core'
-import { RouterModule } from '@angular/router'
+import { Component, OnInit, inject } from '@angular/core'
+import { RouterModule, Router, NavigationEnd } from '@angular/router'
+import { CommonModule } from '@angular/common'
+import { filter } from 'rxjs/operators'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, CommonModule],
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  isHome = true
+  private router = inject(Router)
+
+  ngOnInit() {
+    this.router.events
+      .pipe(filter((event) => event instanceof NavigationEnd))
+      .subscribe((event: NavigationEnd) => {
+        this.isHome = event.urlAfterRedirects === '/'
+      })
+  }
+}
